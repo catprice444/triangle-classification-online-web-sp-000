973a05234137489f9ffab95a63b7507b9f65ba5f
Catherine Price <catprice444@gmail.com>
2020-06-01 19:23:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,10 +17,10 @@ class Triangle
   #
   # 3.  `:scalene`
   def kind
-    if @side_2 == @side_3 || @side_1 == @side_2 || @side_1 == @side_3
-      :isosceles
-    elsif @side_2 == @side_1 && @side_3 == @side_1
+    if @side_2 == @side_1 && @side_3 == @side_1
       :equilateral
+    elsif @side_2 == @side_3 || @side_1 == @side_2 || @side_1 == @side_3
+        :isosceles
     else
       :scalene
     end
